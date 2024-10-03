bd78786c2f0df0ef91cb5fd70bceff9b87871fe1
Kunal Jolly Saxena <134881423+Kunaljolly@users.noreply.github.com>
2024-10-03 13:26:12 +0530

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,4 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {x: i + 1 for i, x in enumerate(sorted(set(arr)))}
+        return [rank[x] for x in arr]
